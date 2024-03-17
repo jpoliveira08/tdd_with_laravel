00e639eba6bb794aa1ba02a3ca14222b5fc7cbb8
jpoliveira08 <joaopedro.oliveirajava@gmail.com>
2024-03-16 21:15:43 -0300

    Setting up the dev environment

---
+++ app/app/Models/Check.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Check extends Model
+{
+    use HasFactory;
+
+    /**
+     * Enable mass assignment for all the fields
+     *
+     * @var array
+     */
+    protected $guarded = [];
+}
